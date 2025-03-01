3da3a6ead9a0921e4f67749675810170fa15e1dc
R.G. <frontend-rob@proton.me>
2025-03-01 20:15:13 +0100

    fix(marquee): delete hero-marquee component

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
-import { HeroMarqueeComponent } from './hero-marquee/hero-marquee.component';
-import { ProfileComponent } from './profile/profile.component';
 import { SkillsetComponent } from './skillset/skillset.component';
+import { ProfileComponent } from './profile/profile.component';
 import { SpotlightComponent } from './spotlight/spotlight.component';
 import { TestimonialsComponent } from './testimonials/testimonials.component';
 import { FooterCtaComponent } from '../../shared/footer-cta/footer-cta.component';
@@ -11,9 +10,8 @@ import { FooterCtaComponent } from '../../shared/footer-cta/footer-cta.component
     selector: 'app-about',
     imports: [
         HeroComponent,
-        HeroMarqueeComponent,
-        ProfileComponent,
         SkillsetComponent,
+        ProfileComponent,
         SpotlightComponent,
         TestimonialsComponent,
         FooterCtaComponent
