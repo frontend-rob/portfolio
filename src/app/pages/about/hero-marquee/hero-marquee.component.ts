86d42bd61565e74bdab8cd59699aeffd9669e6b0
R.G. <frontend-rob@proton.me>
2025-03-01 15:46:10 +0100

    feat(marquee): added marquee in hero section

--- src/app/pages/about/hero-marquee/hero-marquee.component.ts
+++ src/app/pages/about/hero-marquee/hero-marquee.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-hero-marquee',
-  imports: [],
-  templateUrl: './hero-marquee.component.html',
-  styleUrl: './hero-marquee.component.scss'
+    selector: 'app-hero-marquee',
+    imports: [CommonModule],
+    templateUrl: './hero-marquee.component.html',
+    styleUrls: ['./hero-marquee.component.scss']
 })
-export class HeroMarqueeComponent {
 
+export class HeroMarqueeComponent {
+    marqueeItems = [
+        'Item 1',
+        'Item 2',
+        'Item 3',
+        'Item 4',
+        'Item 5',
+        'Item 6'
+    ];
+
+    get marqueeItemsList() {
+        const repetitions = 3;
+        let items: string[] = [];
+        for (let i = 0; i < repetitions; i++) {
+            items = items.concat(this.marqueeItems);
+        }
+        return items;
+    }
 }
