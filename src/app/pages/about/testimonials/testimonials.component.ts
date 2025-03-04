1c75a39d5ebd0d4f66024b642db0460070625896
R.G. <frontend-rob@proton.me>
2025-03-04 20:04:39 +0100

    feat(testimonials): add testimonial section

--- src/app/pages/about/testimonials/testimonials.component.ts
+++ src/app/pages/about/testimonials/testimonials.component.ts
@@ -1,12 +1,133 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChildren, QueryList, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
     selector: 'app-testimonials',
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './testimonials.component.html',
     styleUrl: './testimonials.component.scss'
 })
 
-export class TestimonialsComponent {
+export class TestimonialsComponent implements AfterViewInit, OnDestroy {
+    testimonials: { name: string, role: string, testimonial: string }[] = [
+        {
+            name: 'Name 1',
+            role: 'Role of Person',
+            testimonial: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores nobis omnis ad deserunt iure vel atque nihil vitae officia quam nemo minima modi repellendus ut impedit repellat delectus, corrupti tenetur.'
+        },
+        {
+            name: 'Name 2',
+            role: 'Role of Person',
+            testimonial: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores nobis omnis ad deserunt iure vel atque nihil vitae officia quam nemo minima modi repellendus ut impedit repellat delectus, corrupti tenetur.'
+        },
+        {
+            name: 'Name 3',
+            role: 'Role of Person',
+            testimonial: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores nobis omnis ad deserunt iure vel atque nihil vitae officia quam nemo minima modi repellendus ut impedit repellat delectus, corrupti tenetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores nobis omnis ad deserunt iure vel atque nihil vitae officia quam nemo minima modi repellendus ut impedit repellat delectus, corrupti tenetur.'
+        },
+        {
+            name: 'Name 4',
+            role: 'Role of Person',
+            testimonial: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores nobis omnis ad deserunt iure vel atque nihil vitae officia quam nemo minima modi repellendus ut impedit repellat delectus, corrupti tenetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores nobis omnis ad deserunt iure vel atque nihil vitae officia quam nemo minima modi repellendus ut impedit repellat delectus, corrupti tenetur.'
+        },
+        {
+            name: 'Name 5',
+            role: 'Role of Person',
+            testimonial: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores nobis omnis ad deserunt iure vel atque nihil vitae officia quam nemo minima modi repellendus ut impedit repellat delectus, corrupti tenetur.'
+        },
+    ];
 
+    currentPage: number = 1;
+    expanded: boolean = false;
+    isOverflowing: boolean[] = [];
+    direction: 'next' | 'prev' = 'next';
+    transitionEnabled = true;
+    autoSlideInterval: any;
+
+    @ViewChildren('testimonialContent') testimonialContents!: QueryList<ElementRef>;
+
+    ngAfterViewInit() {
+        this.checkOverflow();
+        this.startAutoSlide();
+    }
+
+    ngOnDestroy() {
+        this.stopAutoSlide();
+    }
+
+    get loopedTestimonials() {
+        return [
+            this.testimonials[this.testimonials.length - 1],
+            ...this.testimonials,
+            this.testimonials[0]
+        ];
+    }
+
+    nextPage() {
+        this.updatePage(this.currentPage + 1, 1);
+        this.resetAutoSlide();
+    }
+
+    prevPage() {
+        this.updatePage(this.currentPage - 1, this.loopedTestimonials.length - 2);
+        this.resetAutoSlide();
+    }
+
+    updatePage(newPage: number, resetPage: number) {
+        this.transitionEnabled = true;
+        this.currentPage = newPage;
+
+        if (newPage >= this.loopedTestimonials.length - 1 || newPage <= 0) {
+            setTimeout(() => {
+                this.transitionEnabled = false;
+                this.currentPage = resetPage;
+            }, 500);
+        }
+        this.expanded = false;
+        this.checkOverflow();
+    }
+
+    getDisplayPage() {
+        if (this.currentPage === 0) {
+            return this.testimonials.length;
+        } else if (this.currentPage > this.testimonials.length) {
+            return 1;
+        } else {
+            return this.currentPage;
+        }
+    }
+
+    getTransform() {
+        return `translateX(-${this.currentPage * 100}%)`;
+    }
+
+    toggleExpand() {
+        this.expanded = !this.expanded;
+    }
+
+
+    checkOverflow() {
+        setTimeout(() => {
+            this.isOverflowing = this.testimonialContents.toArray().map(element => {
+                return element.nativeElement.scrollHeight > element.nativeElement.clientHeight;
+            });
+        });
+    }
+
+    startAutoSlide() {
+        this.autoSlideInterval = setInterval(() => {
+            this.nextPage();
+        }, 10000);
+    }
+
+    stopAutoSlide() {
+        if (this.autoSlideInterval) {
+            clearInterval(this.autoSlideInterval);
+        }
+    }
+
+    resetAutoSlide() {
+        this.stopAutoSlide();
+        this.startAutoSlide();
+    }
 }
